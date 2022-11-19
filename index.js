fbcf765f108c586782bee494a042187f4cdb478f
mosesochacha <ochachamoses443@gmail.com>
2022-11-19 14:10:02 +0300

    Complited assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+const add =function(a="8",b=8){
+return a+b
+}
+add(8+8);
+const subtract =function(a="95",b ="88"){
+    return a-b
+}
+const multiply =function ( a="5",b="20"){
+    return a*b
+}
+const divide =function ( a="10000" ,b="20"){
+return a/b
+}
+const increment =function (n =""){
+    return (n+=1);
+}
+const decrement =function (n=""){
+    return(n-=1)
+}
+function makeInt(n) {
+    return parseFloat(n) * 1.0* 1
+}
+function preserveDecimal (n) {
+    return parseFloat(n) * 1.0 * 1
+}
+
+    
